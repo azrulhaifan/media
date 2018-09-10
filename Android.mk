a4fdb13fc8c393b010e25f8fa328a3fdc1f39b07
azrulhaifan <ahaifan@gmail.com>
2018-09-11 01:02:36 +0700

    Revert "fix build for pa"
    This reverts commit f9bb3c96acb8b1b767e7ea42b7cd1bef81f3e5cc.

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -11,3 +12,4 @@ endif
 
 include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
 
+endif
